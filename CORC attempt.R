c26f06d583751baccd42f151d8b27ec9ecac644e
aidan-r <66278865+aidan-r@users.noreply.github.com>
2020-06-15 12:10:36 -0400

    Update CORC attempt.R

--- CORC attempt.R
+++ CORC attempt.R
@@ -23,4 +23,51 @@ fit_experiments <- defineExperiments(
 )
 
 #define the parameters for COPASI
-fit_parameters <- 
+fit_parameters <-
+  map(
+    parameter_strict(regex(c("V1$", "V2$", "V5$", "V6$", "V9$", "V10$"))),
+    ~ {
+      val <- getParameters(.x)$value
+      defineParameterEstimationParameter(parameter(.x, "Value"), start_value = val, lower_bound = val * 0.1, upper_bound = val * 1.9)
+    }
+  )
+
+result <- 
+  runParameterEstimation(
+    parameters = fit_parameters,
+    experiments = fit_experiments,
+    method = list(method = "LevenbergMarquardt",
+                  log_verbosity = 2),
+    update_model = TRUE
+  )
+
+data_after <- runTimeCourse(1000,1)$result
+
+plots <- list(
+  `Erk2-P` =
+    ggplot(mapping = aes(x = Time, y = `Erk2-P`)) +
+    geom_point(data = data_experimental, aes(color = "experimental")) +
+    geom_line(data = data_before, aes(color = "before")) +
+    geom_line(data = data_after, aes(color = "after")) +
+    scale_color_manual(values = c(before = "red", after = "green", experimental = "black")) +
+    labs(
+      x = paste0("Time (", getTimeUnit(), ")"),
+      y = paste0("Erk2-P (", getQuantityUnit(), ")"),
+      color = "Series"
+    ),
+  `Mos-P` =
+    ggplot(mapping = aes(x = Time, y = `Mos-P`)) +
+    geom_point(data = data_experimental, aes(color = "experimental")) +
+    geom_line(data = data_before, aes(color = "before")) +
+    geom_line(data = data_after, aes(color = "after")) +
+    scale_color_manual(values = c(before = "red", after = "green", experimental = "black")) +
+    labs(
+      x = paste0("Time (", getTimeUnit(), ")"),
+      y = paste0("Mos-P (", getQuantityUnit(), ")"),
+      color = "Series"
+    )
+)
+
+unloadModel()
+
+result$fitted_values
