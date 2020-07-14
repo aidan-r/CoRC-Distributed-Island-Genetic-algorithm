18176b9b5363e8ed5f7261842c4862b85fa77ade
Aidan-R <aidan.riley@uconn.edu>
2020-07-14 17:40:30 -0400

    Adjusted to run and record 10 rounds of the GA search for the KinMMFit Model

--- Genetic Island Algorithm.R
+++ Genetic Island Algorithm.R
@@ -121,10 +121,35 @@ set_nth_individual <- function(method, index, ind)
   method_set_nth_population(method, index, vec)
 }
 
+#COMMENT: I think this function should be declared outside the loop... 
+exchange <- function(met_a,met_b){
+  #initializing populations from method object
+  pop_a <- get_population(met_a) 
+  pop_b <- get_population(met_b)
+  
+  #generating the random index
+  idx <- sample(1:(dim(pop_a)[1]),1,replace = TRUE) 
+  #retrieving the randomly selected individuals
+  random_indiv_b <- pop_b[idx,]
+  random_indiv_a <- pop_a[idx,]
+  
+  #actually completing the exchange
+  set_nth_individual(met_a,idx-1,random_indiv_b)#replacing a random individual in population one with the best from population 
+  #I am pretty sure this is where the "-1" needs to be placed to function correctly
+  set_nth_individual(met_b,idx-1,random_indiv_a)#replacing a random individual in population one with the best from population 
+}
+
+
 library(CoRC)
 
-c_datamodel_one <- loadModel("C:/Users/aidan/Desktop/HRP/CoRC-Distributed-Island-Genetic-Algorithm/KinMMFit.cps")
-c_datamodel_two <- loadModel("C:/Users/aidan/Desktop/HRP/CoRC-Distributed-Island-Genetic-Algorithm/KinMMFit.cps")
+# we create a base name, so that all filenames can be derived from it
+basename <- "C:/Users/aidan/Desktop/HRP/CoRC-Distributed-Island-Genetic-Algorithm/KinMMFit"
+
+# filename of model file to load; this just appends ".cps" to the basename
+modelfile <- sprintf("%s.cps",basename)
+
+c_datamodel_one <- loadModel(modelfile)
+c_datamodel_two <- loadModel(modelfile)
 
 task_one <- as(c_datamodel_one$getTask("Parameter Estimation"), "_p_CFitTask")
 
@@ -166,112 +191,103 @@ if (method_two$getObjectName() != "Genetic Algorithm")
   stop("This code only works with Genetic Algorithm")
 }
 
-method_one$optimise_start() # initialize populations
-method_two$optimise_start()
-
-print(paste("population size of instance one ", method_one$getPopulationSize()))
-print(paste("generation number of instance one", method_one$getNumGenerations()))
-
-print(paste("population size of instance two ", method_two$getPopulationSize()))
-print(paste("generation number of instance two", method_two$getNumGenerations()))
-
+#extract the number of generations
+method_one$optimise_start() 
 generations <- method_one$getNumGenerations()
-steps <- 10
-
-
-print("Initial population One")
 initial_pop_one <- get_population(method_one)
-print (initial_pop_one)
-
-
-print("Initial population Two")
-initial_pop_two <- get_population(method_two)
-print (initial_pop_two)
-
-
-
-
-exchange <- function(met_a,met_b){
-  #initializing populations from method object
-  pop_a <- get_population(met_a) 
-  pop_b <- get_population(met_b)
-  
-  #generating the random index
-  idx <- sample(1:(dim(pop_a)[1]),1,replace = TRUE) 
-  #retrieving the randomly selected individuals
-  random_indiv_b <- pop_b[idx,]
-  random_indiv_a <- pop_a[idx,]
-  
-  #actually completing the exchange
-  set_nth_individual(met_a,idx-1,random_indiv_b)#replacing a random individual in population one with the best from population 
-  #I am pretty sure this is where the "-1" needs to be placed to function correctly
-  set_nth_individual(met_b,idx-1,random_indiv_a)#replacing a random individual in population one with the best from population 
-  
-  
-}
-
-#need a column for the gen number, the objective value, and each parameter that was estimated
-#start off with 0 rows and successively add at each step
-
-col <- 2 + dim(initial_pop_one)[2]
-
-output_one <- data.frame(matrix(ncol = col,nrow = generations)) #dataframe for the first pop
-output_two <- data.frame(matrix(ncol = col,nrow = generations)) #dataframe for the second pop
-
-#encountering some type of error with the function correctly appending to the dataframe
-
-
-#main loop that will run both populations through the ga while exchanging random values
-for(i in 1:generations){
-  if(i %% steps == 0){
-    #exchange the values between the two populations
-    exchange(method_one,method_two)
-      
-    #step population 1 and save the data
-    method_one$optimise_step()
-    obj_values_one <- get_objective_values(method_one) #retrieve the objective values
-    population_one <- get_population(method_one) #retrieve the current population
-    best_index_one <- method_one$getBestIndex() #retrieve the index of the best member of the population
-    obj_values_one[best_index_one+1]
-    population_one[best_index_one+1,]
-    output_one[i,] <- c(best_index_one+1,obj_values_one[best_index_one+1],population_one[best_index_one+1,]) #assign the current row of the data frame to the best value
-      
-    #step population 2 and save the data
-    method_two$optimise_step()
-    obj_values_two <- get_objective_values(method_two)
-    population_two <- get_population(method_two)
-    best_index_two = method_two$getBestIndex()
-    obj_values_two[best_index_two+1]
-    population_two[best_index_two+1,]
-    output_two[i,] <- c(best_index_two+1,obj_values_two[best_index_two+1],population_two[best_index_two+1,])
-    }
-    else{
-      method_one$optimise_step()
-      obj_values_one <- get_objective_values(method_one)
-      population_one <- get_population(method_one)
-      best_index_one = method_one$getBestIndex()
-      obj_values_one[best_index_one+1]
-      population_one[best_index_one+1,]
-      output_one[i,] <- c(best_index_one+1,obj_values_one[best_index_one+1],population_one[best_index_one+1,])
-        
-      method_two$optimise_step()
-      obj_values_two <- get_objective_values(method_two)
-      population_two <- get_population(method_two)
-      best_index_two = method_two$getBestIndex()
-      obj_values_two[best_index_two+1]
-      population_two[best_index_two+1,]
-      output_two[i,] <- c(best_index_two+1,obj_values_two[best_index_two+1],population_two[best_index_two+1,])
-        
-    }
-  }
-
-
-
-
-# population is no longer available after calling finish
+col <- 4 + dim(initial_pop_one)[2]
 method_one$optimise_finish();
-method_two$optimise_finish()
-print("output one is")
-view(output_one)
-print("output two is")
-view(output_two)
+# repeats is the number of times we want to repeat the whole process
+# later, this could be read from the command line instead of being hard-coded
+repeats <- 10
+
+#need a column for the iteration, island, gen number, the objective value, and each parameter that was estimated
+#start off with 0 rows and successively add at each step
+# create a data frame for all the results
+output <- data.frame(matrix(ncol = col,nrow = generations*repeats*2)) #dataframe
+
+
+
+# this is the outer loop that will repeat the parameter estimation many times
+for(r in 1:repeats){  
+
+ method_one$optimise_start() # initialize populations
+ method_two$optimise_start()
+
+ print(paste("population size of instance one ", method_one$getPopulationSize()))
+ print(paste("generation number of instance one", method_one$getNumGenerations()))
+
+ print(paste("population size of instance two ", method_two$getPopulationSize()))
+ print(paste("generation number of instance two", method_two$getNumGenerations()))
+ 
+ #retrieve the correct number of generations for the loop to iterate through from the COPASI file
+ generations <- method_one$getNumGenerations()
+ #set the number of steps before exchanging population individuals
+ steps <- 10
+
+ print("Initial population One")
+ initial_pop_one <- get_population(method_one)
+ print (initial_pop_one)
+
+ print("Initial population Two")
+ initial_pop_two <- get_population(method_two)
+ print (initial_pop_two)
+
+
+ #main loop that will run both populations through the ga while exchanging random values
+ for(i in 1:generations){
+   row_n <- (r-1)*2*generations + (2*i)
+   if(i %% steps == 0){
+     #exchange the values between the two populations
+     exchange(method_one,method_two)
+      
+     #step population 1 and save the data
+     method_one$optimise_step()
+     obj_values_one <- get_objective_values(method_one) #retrieve the objective values
+     population_one <- get_population(method_one) #retrieve the current population
+     best_index_one <- method_one$getBestIndex() #retrieve the index of the best member of the population
+     obj_values_one[best_index_one+1]
+     population_one[best_index_one+1,]
+     output[row_n-1,] <- c(r,1,best_index_one+1,obj_values_one[best_index_one+1],population_one[best_index_one+1,]) #assign the current row of the data frame to the best value
+      
+     #step population 2 and save the data
+     method_two$optimise_step()
+     obj_values_two <- get_objective_values(method_two)
+     population_two <- get_population(method_two)
+     best_index_two = method_two$getBestIndex()
+     obj_values_two[best_index_two+1]
+     population_two[best_index_two+1,]
+     output[row_n,] <- c(r,2,best_index_two+1,obj_values_two[best_index_two+1],population_two[best_index_two+1,])
+     }
+     else{
+       method_one$optimise_step()
+       obj_values_one <- get_objective_values(method_one)
+       population_one <- get_population(method_one)
+       best_index_one = method_one$getBestIndex()
+       obj_values_one[best_index_one+1]
+       population_one[best_index_one+1,]
+       output[row_n-1,] <- c(r,1,best_index_one+1,obj_values_one[best_index_one+1],population_one[best_index_one+1,])
+         
+       method_two$optimise_step()
+       obj_values_two <- get_objective_values(method_two)
+       population_two <- get_population(method_two)
+       best_index_two = method_two$getBestIndex()
+       obj_values_two[best_index_two+1]
+       population_two[best_index_two+1,]
+       output[row_n,] <- c(r,2,best_index_two+1,obj_values_two[best_index_two+1],population_two[best_index_two+1,])
+     }
+   }
+  
+ # population is no longer available after calling finish
+ method_one$optimise_finish();
+ method_two$optimise_finish()
+ print("output one is")
+ view(output[r,1])
+ print("output two is")
+ view(output)
+} # end of the outer loop
+
+# now we need to save the entire data frame!
+outfilename <- sprintf("%s-%d.tsv",basename,repeats)
+write_tsv(x = output, path = outfilename, col_names = TRUE)
+
