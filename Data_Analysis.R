e94dc862ea7879a2f4635cb0453406f83caeb52e
Aidan-R <aidan.riley@uconn.edu>
2020-07-16 17:23:22 -0400

    Generalized to allow various iterations

--- Data_Analysis.R
+++ Data_Analysis.R
@@ -1,17 +1,39 @@
 library(tidyverse)
 
 #cannot import with fewer column names
-Standard_ga <- read_tsv("C:/Users/aidan/Desktop/HRP/CoRC-Distributed-Island-Genetic-algorithm/GA Performance Data/Standard_GA_10_gens.txt")
-
-#have to import data manually
-#wondering how to set when the value gets recorded in COPASI
+Standard_ga <- read_tsv("C:/Users/aidan/Desktop/HRP/CoRC-Distributed-Island-Genetic-algorithm/GA Performance Data/Standard_GA_10_gens.txt",col_names = FALSE)
 
 
-#Remove the parentheses columns from the data frame
-#Standard_GA_300_20 = subset(Standard_GA_300_20, select = -c(V3,V11))
+#remove the bracket columns from the df
+for(j in 1:dim(Standard_ga)[2]){
+  if(j > dim(Standard_ga)[2]){
+    break}
+  
+  else if(Standard_ga[1,j] == "("|Standard_ga[1,j] == ")"){
+    Standard_ga <- Standard_ga%>%select(-j)
+  }
+}
+#add a column of all zeros for mutation
+Standard_ga <-Standard_ga %>%
+    mutate(iteration = 0)
 
-ggplot(data = Standard_GA_300_20,mapping = aes(x = Standard_GA_300_20[,1],y = Standard_GA_300_20[,2])) +
-  geom_point()
+#Separate the iterations of the Standard Search
+val=1
+for(i in 1:dim(Standard_ga)[1]){
+  if(i == 1){
+    Standard_ga[i,dim(Standard_ga)[2]] <- val
+  }
+  else if(Standard_ga[i,2] <= Standard_ga[i-1,2]){
+    Standard_ga[i,dim(Standard_ga)[2]] <- val
+  }
+  else{
+    val <- val+1
+    Standard_ga[i,dim(Standard_ga)[2]] <- val
+  }
+}
+
+
+
 
 #retrieving the data from the island script
 df_1 <- read_tsv("C:/Users/aidan/Desktop/HRP/CoRC-Distributed-Island-Genetic-algorithm/KinMMFit-10.tsv")
@@ -22,63 +44,66 @@ iter <- df_1%>%
 iter <- as.double(iter)
 
 #separating the objective values of the two islands across ten trials to recreate the COPASI plot
-#need to be able to plot which currently cannot
 isle_1<- df_1%>%filter(island==1)%>%select(evals,fitness)
 isle_2<- df_1 %>%filter(island==2)%>% select(evals,fitness)
 
 
 
-df_1 %>%
-  group_by(X1)%>%
-  filter(X2 == 1)%>%
-  summarise(avg = mean(X4))
+#initialize a data frame for the number of iterations + the function evaluations + stdev + mean
+island_1_avg <- data.frame(matrix(ncol = iter,nrow = (dim(isle_1)[1]/iter)))
+fitness_1 <- df_1%>%
+  filter(island == 1)%>%
+  select(iteration,fitness)
+row_count = 1
 
-df_1 %>%
-  group_by(X1)%>%
-  filter(X2 == 2)%>%
-  summarise(avg = mean(X4))
+#Transfer the Fitness values of every iteration into a usable DataFrame
+for(i in 1:dim(fitness_1)[1]){
+  col <- as.double(fitness_1[i,1])
+  if(row_count == as.double(dim(fitness_1)[1]/ iter)){
+    island_1_avg[row_count,col] <- as.double(fitness_1[i,2])
+    row_count = 1
+    }
+  else{
+    island_1_avg[row_count,col] <- as.double(fitness_1[i,2])
+    row_count <- row_count + 1}
+}
 
-#creates a new df where the columns represent the fitness values of a specific iteration, calculates the mean for plotting
-
-#This is not functioning as intended
-test <- df_1 %>%
-filter(island == 1)%>%
-select(evals,fitness)
-for(i in 1:iter){
-  test<-mutate(paste("iteration_",i)=df%>%filter(island == 1,iteration == i)%>%select(fitness))}
-  
-
-
-island_1_avg <- df_1 %>% 
-  filter(island ==1,iteration == 1)%>%
-  select(evals,fitness)%>%
-  mutate(iteration_2 = df_1%>%filter(island ==1,iteration == 2)%>%select(fitness),
-         iteration_3 = df_1%>%filter(island ==1,iteration == 3)%>%select(fitness),
-         iteration_4 = df_1%>%filter(island ==1,iteration == 4)%>%select(fitness),
-         iteration_5 = df_1%>%filter(island ==1,iteration == 5)%>%select(fitness),
-         iteration_6 = df_1%>%filter(island ==1,iteration == 6)%>%select(fitness),
-         iteration_7 = df_1%>%filter(island ==1,iteration == 7)%>%select(fitness),
-         iteration_8 = df_1%>%filter(island ==1,iteration == 8)%>%select(fitness),
-         iteration_9 = df_1%>%filter(island ==1,iteration == 9)%>%select(fitness),
-         iteration_10 = df_1%>%filter(island ==1,iteration == 10)%>%select(fitness))
-island_1_avg <- island_1_avg%>%mutate(mean = rowMeans(island_1_avg%>%select(!evals)))
-
-island_2_avg <- df_1 %>% 
-  filter(island ==2,iteration == 1)%>%
-  select(evals,fitness)%>%
-  mutate(iteration_2 = df_1%>%filter(island ==2,iteration == 2)%>%select(fitness),
-         iteration_3 = df_1%>%filter(island ==2,iteration == 3)%>%select(fitness),
-         iteration_4 = df_1%>%filter(island ==2,iteration == 4)%>%select(fitness),
-         iteration_5 = df_1%>%filter(island ==2,iteration == 5)%>%select(fitness),
-         iteration_6 = df_1%>%filter(island ==2,iteration == 6)%>%select(fitness),
-         iteration_7 = df_1%>%filter(island ==2,iteration == 7)%>%select(fitness),
-         iteration_8 = df_1%>%filter(island ==2,iteration == 8)%>%select(fitness),
-         iteration_9 = df_1%>%filter(island ==2,iteration == 9)%>%select(fitness),
-         iteration_10 = df_1%>%filter(island ==2,iteration == 10)%>%select(fitness))
-island_2_avg <- island_2_avg%>%mutate(mean = rowMeans(island_2_avg%>%select(!evals)))
+island_1_avg <-island_1_avg%>%
+  mutate(avg = rowMeans(island_1_avg))
 
 
 
+#Repeat for Island 2
+island_2_avg <- data.frame(matrix(ncol = iter,nrow = (dim(isle_2)[1]/iter)))
+fitness_2 <- df_1%>%
+  filter(island == 2)%>%
+  select(iteration,fitness)
+  row_count = 1
+for(i in 1:dim(fitness_2)[1]){
+  col <- as.double(fitness_2[i,1])
+  if(row_count == as.double(dim(fitness_2)[1]/ iter)){
+    island_2_avg[row_count,col] <- as.double(fitness_2[i,2])
+    row_count = 1
+  }
+  else{
+    island_2_avg[row_count,col] <- as.double(fitness_2[i,2])
+    row_count <- row_count + 1}
+}
+
+island_2_avg <-island_2_avg%>%
+  mutate(avg = rowMeans(island_2_avg))
+
+#create an x axis vector
+axis <- isle_1 %>%
+  select(evals)%>%
+  filter(evals <= 6040)
+
+#Add the axis vector to the islands
+island_1_avg <- island_1_avg%>%
+  mutate(axis)
+island_2_avg <- island_2_avg%>%
+  mutate(axis)  
+    
 
 #create the graph for island one from COPASI  
 ggplot(data = isle_1,mapping = aes(x = evals,y = fitness)) + 
@@ -90,13 +115,24 @@ ggplot(data = isle_2,mapping = aes(x = evals,y = fitness)) +
   geom_point()+
   scale_y_log10()
 
-#plot the average progression from island 1
-ggplot(data = island_1_avg,mapping = aes(x = evals,y=mean))+
-  geom_point()+
+#plot the average progression from island 1 and 2 together
+figure_avgs <- ggplot()+
+  geom_point(data = island_1_avg,mapping = aes(x = evals,y = avg),color = 'blue')+
+  geom_point(data = island_2_avg,mapping = aes(x = evals,y = avg),color = 'red')+
   scale_y_log10()
 
-#plot the average progression from island 2
-ggplot(data=island_2_avg,mapping = aes(x = evals,y=mean))+
+print(figure_avgs)
+
+#plot progression of both islands together
+figure_islands <- ggplot()+
+  geom_point(data = isle_1,mapping = aes(x = evals,y=fitness),color = 'blue')+
+  geom_point(data = isle_2,mapping = aes(x = evals,y = fitness,color = 'red'))+
+  scale_y_log10()
+print(figure_islands)
+
+  
+#Plot the progress of the standard GA
+ggplot(data = Standard_ga,mapping = aes(x = X1,y =X2))+
   geom_point()+
   scale_y_log10()
 
